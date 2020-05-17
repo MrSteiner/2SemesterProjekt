910851989a7dbe5b0db7a79353d67f5b5a244463
Noblish <DrNoble2.0@gmail.com>
2020-05-17 13:56:16 +0200

    Slideshow 1
    Generering af slide shows til udstillinger på museum sider

---
+++ Prototype 1.0/js/slides.js
@@ -0,0 +1,25 @@
+var slideIndex = [1];
+
+function plusSlides(s,n) {
+  showSlides(s, slideIndex[s-1] += n);
+}
+
+function currentSlide(s,n) {
+  showSlides(s, slideIndex[s-1] = n);
+}
+
+function showSlides(s,n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides"+s);
+  var dots = document.getElementsByClassName("dot"+s);
+  if (n > slides.length) {slideIndex[s-1] = 1}    
+  if (n < 1) {slideIndex[s-1] = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex[s-1]-1].style.display = "block";  
+  dots[slideIndex[s-1]-1].className += " active";
+}
